b43c06fb2b909b32b659dc8375aa89a89581b0f5
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2025-01-03 16:35:00 +0000

    adding comments to function
    Working through course adding some comments to the functions to explain the working

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -5,15 +5,19 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the serverside', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the serverside', app: 'Natours' });
+// });
 
 // app.post('/', (req, res) => {
 //   res.send('You can post to this endpoint......');
 // });
+
+// -------- To start server for postman testing use nodemon app.js -----------
+
+// This is sync so will block but its okay as its at the top level and not within a callback function
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -29,17 +33,20 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.get('/api/v1/tours/:id', (req, res) => {
+  // Shows the params that are passed in the url with :name etc
   console.log(req.params);
+  // attach the searched ID to a new variable, * 1 results in the string being converted to a number
   const id = req.params.id * 1;
+  // Find method goes through the tours array and looks to see if the ID matches the searched number
   const tour = tours.find((el) => el.id === id);
-  //   if (id > tours.length) {
+  //   If there is no ID that matches
   if (!tour) {
     return res.status(404).json({
       status: 'Failed',
       message: 'Invalid ID',
     });
   }
-
+  // If the ID matches the searched input
   res.status(200).json({
     status: 'success',
     data: {
